8797630d58f749611c0d3cd426380a7b37505955
dries.k <2129561-kdries@users.noreply.gitlab.com>
2020-05-18 17:33:04 +0200

    add benchmarks

--- c-fractals/benchmark/bench_avx_threaded.c
+++ c-fractals/benchmark/bench_avx_threaded.c
@@ -1,18 +1,12 @@
-#include "main.h"
+#include "benchmarks.h"
 
 
-#if (!defined(TEST) && !defined(SHARED) && !defined(BENCHMARK))
-int
-main(int argc, char * argv[])
-{    
-    FRACDTYPE c_real = -0.835f;
-	FRACDTYPE c_imag = -0.2321f;
+BENCH_FUNC(bench_avx_threaded) {
+    FRACDTYPE c_real = C_REAL;
+	FRACDTYPE c_imag = C_IMAG;
 
-    FRACDTYPE _Complex c = -0.835 - 0.2321*I;
+    FRACDTYPE _Complex c = c_real + c_imag*I;
     FRACDTYPE R = ceilf(cabs(c)) + 1;
-
-    int ROWS = 8*100;
-    int COLS = 8*100;
 
     HCMATRIX hCmatrix = fractal_cmatrix_create(1, 1);
     hCmatrix = fractal_cmatrix_reshape(hCmatrix, ROWS, COLS);
@@ -25,8 +19,6 @@ main(int argc, char * argv[])
 
     FRACDTYPE x_step = (x_end - x_start) / ROWS;
     FRACDTYPE y_step = (y_end - y_start) / COLS;
-
-    int max_iterations = 1000;
     
     struct FractalProperties fp = {
         .x_start = -R,
@@ -37,17 +29,10 @@ main(int argc, char * argv[])
         .c_real = c_real,
         .c_imag = c_imag,
         .R = R,
-        .max_iterations = max_iterations,
+        .max_iterations = MAX_ITERATIONS,
     };
 
-    //fractal_get_colors(hCmatrix, &fp);
-    //fractal_get_colors_th(hCmatrix, &fp, 6);
-    //fractal_avxf_get_colors(hCmatrix, &fp);
-    fractal_avxf_get_colors_th(hCmatrix, &fp, 6);
-
-    //FRACDTYPE max_color = fractal_cmatrix_max(hCmatrix);
+    fractal_avxf_get_colors_th(hCmatrix, &fp, NUM_THREADS);
 
     fractal_cmatrix_free(hCmatrix);
-	return 0;
 }
-#endif
