6783e6674916f90fc91328ab9c1247e00c07adf6
dries.k <k.dries@protonmail.com>
2021-11-07 19:56:29 +0100

    doxygen

--- c-fractals/README.md
+++ c-fractals/README.md
@@ -14,158 +14,81 @@ Include the library interface **fracal_color.h** in the project. Add the source 
 
 ### Functions
 
+- HCMATRIX fractal_cmatrix_create(int height, int width);
+- HCMATRIX fractal_cmatrix_reshape(HCMATRIX hCmatrix, int height_new, int width_new);
+- void fractal_cmatrix_free(HCMATRIX hCmatrix);
+- float * fractal_cmatrix_value(HCMATRIX hCmatrix, int height, int width);
+- void fractal_get_colors(HCMATRIX hCmatrix, struct FractalProperties * fp);
+- void fractal_get_colors_th(HCMATRIX hCmatrix, struct FractalProperties * fp, int num_threads);
+- void fractal_avxf_get_colors(HCMATRIX hCmatrix, struct FractalProperties * fp);
+- void fractal_avxf_get_colors_th(HCMATRIX hCmatrix, struct FractalProperties * fp, int num_threads);
+- float fractal_cmatrix_max(HCMATRIX hCmatrix);
+- void fractal_value_to_color(uint8_t * r, uint8_t * g, uint8_t * b, int value, enum FC_Color color);
+- colorfunc_t fractal_colorfunc_get(enum FC_Color color);
+- void fractal_cmatrix_save(HCMATRIX hCmatrix, const char * filename, enum FC_Color color);
+- void fractal_image_save(uint8_t * image, int width, int height, const char * filename);
+- uint8_t * fractal_image_create(int height, int width);
+- void fractal_image_free(uint8_t * image);
 
-- Initialize a color matrix, returns a handle **HCMATRIX**
+## Example
 
-    ```c
-    HCMATRIX fractal_cmatrix_create(int height, int width);
-    ```
-
-- Reshape an existing color matrix
-
-    ```c
-    HCMATRIX fractal_cmatrix_reshape(HCMATRIX hCmatrix, int height_new, int width_new);
-    ```
-
-- Free a color matrix
-
-    ```c
-    void fractal_cmatrix_free(HCMATRIX hCmatrix);
-    ```
-
-- Retrieve a pointer to a value in the color matrix
-
-    ```c
-    float * fractal_cmatrix_value(HCMATRIX hCmatrix, int height, int width);
-    ```
-
-- Compute the fractal colors
-
-    ```c
-    void fractal_get_colors(HCMATRIX hCmatrix, struct FractalProperties * fp);
-    ```
-
-- Compute the fractal colors with threads
-
-    ```c
-    void fractal_get_colors_th(HCMATRIX hCmatrix, struct FractalProperties * fp, int num_threads);
-    ```
-
-- Compute the fractal colors with AVX2
-
-    ```c
-    void fractal_avxf_get_colors(HCMATRIX hCmatrix, struct FractalProperties * fp);
-    ```
-
-- Compute the fractal colors with AVX2 and threads
-
-    ```c
-    void fractal_avxf_get_colors_th(HCMATRIX hCmatrix, struct FractalProperties * fp, int num_threads);
-    ```
-
-- Retrieve the maximum color value in the color matrix
-
-    ```c
-    float fractal_cmatrix_max(HCMATRIX hCmatrix);
-    ```
-
-- Convert a cmatrix value to rgb
-
-    ```c
-    void fractal_value_to_color(uint8_t * r, uint8_t * g, uint8_t * b, int value, enum FC_Color color);
-    ```
-
-- Get a color function
-
-    ```c
-    colorfunc_t fractal_colorfunc_get(enum FC_Color color);
-    ```
-
-- Save a color matrix as png
-
-    ```c
-    void fractal_cmatrix_save(HCMATRIX hCmatrix, const char * filename);
-    ```
-
-- Save an image array as png
-
-    ```c
-    void fractal_image_save(uint8_t * image, int width, int height, const char * filename);
-    ```
-
-- Create an image array
-
-    ```c
-    uint8_t * fractal_image_create(int height, int width);
-    ```
-
-- Free an image array
+```c
+int
+main(void)
+{    
+    /* ----------- INPUT PARAMETERS ----------- */
+    float c_real = -0.7835f;
+	float c_imag = -0.2321f;
     
-    ```c
-    void fractal_image_free(uint8_t * image);
-    ```
+    int width = 800;
+    int height = 600;
+
+    float R = 2;
     
-### Structs
+    float aspect_ratio = (float)width/height;
 
-- Parameters to pass to `fractal_get_colors`
+    float x_start = -R;
+    float x_end   =  R;
 
-    ```c
-    struct FractalProperties {
-        float x_start;
-        float x_end;
-        float y_start;
-        float y_end;
-        float width;
-        float height;
-        enum FC_Fractal frac;
-        enum FC_Mode mode;
-        enum FC_Color color;
-        float c_real;
-        float c_imag;
-        float R;
-        int max_iterations;
-        char * lyapunov_sequence;
-        size_t sequence_length;
+    float y_start = x_start/aspect_ratio;
+    float y_end = x_end/aspect_ratio;    
+
+    int max_iterations = 1000;
+
+    enum FC_Mode mode = FC_MODE_JULIA;
+    enum FC_Fractal fractal = FC_FRAC_Z2;
+    enum FC_Color color = FC_COLOR_JET;
+    /* ---------------------------------------- */
+
+
+    struct FractalProperties fp = {
+        .x_start = x_start,
+        .x_end = x_end,
+        .y_start = y_start,
+        .y_end = y_end,
+
+        .width = width,
+        .height = height,
+
+        .frac = fractal,
+        .mode = mode,
+        .color = color,
+
+        .c_real = c_real,
+        .c_imag = c_imag,
+        .R = R,
+        .max_iterations = max_iterations,
     };
-    ```
 
-### Fractals
+    HCMATRIX hCmatrix = fractal_cmatrix_create(height, width);
 
-```c
-enum FC_Fractal {
-    FC_FRAC_Z2,     // z^2 + c
-    FC_FRAC_Z3,     // z^3 + c
-    FC_FRAC_Z4,     // z^4 + c
-    FC_FRAC_ZCONJ2, // (conj(z))^2 + c
-    FC_FRAC_ZCONJ3, // (conj(z))^3 + c
-    FC_FRAC_ZCONJ4, // (conj(z))^4 + c
-    FC_FRAC_ZABS2, // (abs(z_real) + abs(c_real)*j)^2 + c
-    FC_FRAC_ZABS3, // (abs(z_real) + abs(c_real)*j)^3 + c
-    FC_FRAC_ZABS4, // (abs(z_real) + abs(c_real)*j)^4 + c
-};
-```
+    fractal_avxf_get_colors(hCmatrix, &fp);
+    fractal_cmatrix_save(hCmatrix, "fractal_image.png", fp.color);
+    
+    fractal_cmatrix_free(hCmatrix);
 
-### Modes
-
-```c
-enum FC_Mode {
-    FC_MODE_MANDELBROT,
-    FC_MODE_JULIA,
-    FC_MODE_LYAPUNOV,
-};
-```
-
-### Colors
-
-```c
-enum FC_Color {
-    FC_COLOR_ULTRA,
-    FC_COLOR_MONOCHROME,
-    FC_COLOR_TRI,
-    FC_COLOR_JET,
-    FC_COLOR_LAVENDER,
-    FC_COLOR_BINARY,
-};
+	return 0;
+}
 ```
 
 ## Run and test
--- c-fractals/include/fractal_color.h
+++ c-fractals/include/fractal_color.h
@@ -10,23 +10,23 @@
 #include <stdlib.h>
 
 
-/* Fractal functions */
+/** Fractal functions */
 enum FC_Fractal {
-    FC_FRAC_Z2,     // z^2 + c
-    FC_FRAC_Z3,     // z^3 + c
-    FC_FRAC_Z4,     // z^4 + c
-    FC_FRAC_ZCONJ2, // (conj(z))^2 + c
-    FC_FRAC_ZCONJ3, // (conj(z))^3 + c
-    FC_FRAC_ZCONJ4, // (conj(z))^4 + c
-    FC_FRAC_ZABS2,  // (abs(z_real) + abs(c_real)*j)^2 + c
-    FC_FRAC_ZABS3,  // (abs(z_real) + abs(c_real)*j)^3 + c
-    FC_FRAC_ZABS4,  // (abs(z_real) + abs(c_real)*j)^4 + c
-    FC_FRAC_MAGNET, // [(z^2 + c - 1)/(2z + c - 2)]^2
-    FC_FRAC_Z2_Z,   // z^2 + c/z
+    FC_FRAC_Z2,     /**< z^2 + c */
+    FC_FRAC_Z3,     /**< z^3 + c */
+    FC_FRAC_Z4,     /**< z^4 + c */
+    FC_FRAC_ZCONJ2, /**< (conj(z))^2 + c */
+    FC_FRAC_ZCONJ3, /**< (conj(z))^3 + c */
+    FC_FRAC_ZCONJ4, /**< (conj(z))^4 + c */
+    FC_FRAC_ZABS2,  /**< (abs(z_real) + abs(c_real)*j)^2 + c */
+    FC_FRAC_ZABS3,  /**< (abs(z_real) + abs(c_real)*j)^3 + c */
+    FC_FRAC_ZABS4,  /**< (abs(z_real) + abs(c_real)*j)^4 + c */
+    FC_FRAC_MAGNET, /**< [(z^2 + c - 1)/(2z + c - 2)]^2 */
+    FC_FRAC_Z2_Z,   /**< z^2 + c/z */
     FC_FRAC_NUM_ENTRIES,
 };
 
-/* Fractal modes */
+/** Fractal modes */
 enum FC_Mode {
     FC_MODE_MANDELBROT,
     FC_MODE_JULIA,
@@ -34,7 +34,7 @@ enum FC_Mode {
     FC_MODE_NUM_ENTRIES,
 };
 
-/* Color modes to convert CMATRIX values to */
+/** Color modes to convert CMATRIX values to */
 enum FC_Color {
     FC_COLOR_ULTRA,
     FC_COLOR_MONOCHROME,
@@ -51,69 +51,156 @@ typedef void * HCMATRIX;
 typedef void (* colorfunc_t)(uint8_t * r, uint8_t * g, uint8_t * b, int value);
 
 struct FractalProperties {
-    float x_start;
-    float x_end;
-    float y_start;
-    float y_end;
-    int width;
-    int height;
-    enum FC_Fractal frac;
-    enum FC_Mode mode;
-    enum FC_Color color;
-    float c_real;
-    float c_imag;
-    float R;
-    int max_iterations;
-    char * sequence;
-    size_t sequence_length;
+    float x_start;          /**< Lower bound of the real axis in the complex plane */
+    float x_end;            /**< Upper bound of the real axis in the complex plane */
+    float y_start;          /**< Lower bound of the imaginary axis in the complex plane */
+    float y_end;            /**< Upper bound of the imaginary axis in the complex plane */
+    int width;              /**< Width of the image (real axis) */
+    int height;             /**< Height of the image (imaginary axis) */
+    enum FC_Fractal frac;   /**< The fractal function which will be iterated */
+    enum FC_Mode mode;      /**< The fractal mode */
+    enum FC_Color color;    /**< The color mode to render the fractals (required for cuda) */
+    float c_real;           /**< The real part of the c parameter in the fractal function */
+    float c_imag;           /**< The imaginary part of the c parameter in the fractal function */
+    float R;                /**< Escape radius */
+    int max_iterations;     /**< Maximum number of times the fractal function will be iterated */
+    char * sequence;        /**< Lyapunov sequence */
+    size_t sequence_length; /**< Lyapunov sequence length */
     float _x_step;
     float _y_step;
 };
 
 
-/* create a color matrix */
+/**
+ * @brief Initialize a color matrix, returns a handle @c HCMATRIX
+ * 
+ * @param height 
+ * @param width 
+ * @return HCMATRIX 
+ */
 HCMATRIX fractal_cmatrix_create(int height, int width);
 
-/* reshape a color matrix */
+/**
+ * @brief Reshape an existing color matrix, returns a handle @c HCMATRIX to the reshaped color matrix. The original color matrix is freed.
+ * 
+ * @param hCmatrix 
+ * @param height_new 
+ * @param width_new 
+ * @return HCMATRIX 
+ */
 HCMATRIX fractal_cmatrix_reshape(HCMATRIX hCmatrix, int height_new, int width_new);
 
-/* free a color matrix */
+/**
+ * @brief Free a color matrix
+ * 
+ * @param hCmatrix 
+ */
 void fractal_cmatrix_free(HCMATRIX hCmatrix);
 
-/* get a matrix value */
+/**
+ * @brief Retrieve a pointer to a value in the color matrix
+ * 
+ * @param hCmatrix 
+ * @param height 
+ * @param width 
+ * @return float* 
+ */
 float * fractal_cmatrix_value(HCMATRIX hCmatrix, int height, int width);
 
-/* get fractal colors */
+/**
+ * @brief Compute the fractal colors
+ * 
+ * @param hCmatrix 
+ * @param fp 
+ */
 void fractal_get_colors(HCMATRIX hCmatrix, struct FractalProperties * fp);
 
-/* get fractal colors with threading */
+/**
+ * @brief Compute the fractal colors with threads
+ * 
+ * @param hCmatrix 
+ * @param fp 
+ * @param num_threads 
+ */
 void fractal_get_colors_th(HCMATRIX hCmatrix, struct FractalProperties * fp, int num_threads);
 
-/* get fractal colors with AVX2 */
+/**
+ * @brief Compute the fractal colors with AVX2
+ * 
+ * @param hCmatrix 
+ * @param fp 
+ */
 void fractal_avxf_get_colors(HCMATRIX hCmatrix, struct FractalProperties * fp);
 
-/* get fractal colors with AVX2 and threading */
+/**
+ * @brief Compute the fractal colors with AVX2 and threads
+ * 
+ * @param hCmatrix 
+ * @param fp 
+ * @param num_threads 
+ */
 void fractal_avxf_get_colors_th(HCMATRIX hCmatrix, struct FractalProperties * fp, int num_threads);
 
-/* get the maximum color value */
+/**
+ * @brief Retrieve the maximum color value in the color matrix
+ * 
+ * @param hCmatrix 
+ * @return float 
+ */
 float fractal_cmatrix_max(HCMATRIX hCmatrix);
 
-/* save a color matrix as png */
+/**
+ * @brief Save a color matrix as png
+ * 
+ * @param hCmatrix 
+ * @param filename 
+ * @param color 
+ */
 void fractal_cmatrix_save(HCMATRIX hCmatrix, const char * filename, enum FC_Color color);
 
-/* save an image array as png */
+/**
+ * @brief Save an image array as png
+ * 
+ * @param image 
+ * @param width 
+ * @param height 
+ * @param filename 
+ */
 void fractal_image_save(uint8_t * image, int width, int height, const char * filename);
 
-/* convert a cmatrix value to rgb */
+/**
+ * @brief Convert a cmatrix value to rgb
+ * 
+ * @param r 
+ * @param g 
+ * @param b 
+ * @param value 
+ * @param color 
+ */
 void fractal_value_to_color(uint8_t * r, uint8_t * g, uint8_t * b, int value, enum FC_Color color);
 
-/* get a color function */
+/**
+ * @brief Get a color function
+ * 
+ * @param color 
+ * @return colorfunc_t 
+ */
 colorfunc_t fractal_colorfunc_get(enum FC_Color color);
 
-/* create an image array */
+/**
+ * @brief Create an image array
+ * 
+ * @param height 
+ * @param width 
+ * @return uint8_t* 
+ */
 uint8_t * fractal_image_create(int height, int width);
 
-/* free an image array */
+/**
+ * @brief Free an image array
+ * 
+ * @param image 
+ */
 void fractal_image_free(uint8_t * image);
 
 #endif // __FRACTAL_COLOR_H__
